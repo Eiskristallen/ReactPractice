24532a4b9cd2c7dab3ae3aa9797b87fbafb29d9d
Eiskristallen <qhuangb47@gmail.com>
2022-05-11 13:56:12 -0500

    finish maindash part

--- src/components/MainDash/MainDash.jsx
+++ src/components/MainDash/MainDash.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Card from '../Card/Card'
 import './MainDash.css';
-
+import Table from "../Table/Table";
 const MainDash = ()=>{
   return (
     <div className="mainDash">
       <h1>Dash Board</h1>
       <Card/>
+      <Table/>
     </div>
   )
 }
